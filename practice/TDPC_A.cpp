67989c83a7c634d6f4a79c63684b32d93f36f04d
hatano-viviane <rikuhatano09@gmail.com>
2022-04-03 20:44:45 +0900

    Add practice file.

---
+++ practice/TDPC_A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<int> p(N);
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> p[i];
+    }
+
+    int W = 10000;
+    vector<vector<bool>> dp(N + 1, vector<bool>(W + 1, false));
+    dp[0][0] = true;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j <= W; ++j)
+        {
+            if (j >= p[i])
+            {
+                dp[i + 1][j] = dp[i][j] | dp[i][j - p[i]];
+            }
+            else
+            {
+                dp[i + 1][j] = dp[i][j];
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int j = 0; j <= W; ++j)
+    {
+        if (dp[N][j])
+            ++ans;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
