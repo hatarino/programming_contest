3b239b148d1949f8e6257808906c340af2c2286a
hatano-viviane <rikuhatano09@gmail.com>
2022-06-16 02:44:12 +0900

    Add practice file.

--- practice/ABC_153_E.cpp
+++ practice/ABC_153_E.cpp
@@ -13,7 +13,29 @@ int main()
         cin >> A[i] >> B[i];
     }
 
-    int dp[N + 1][H + 1];
+    // 1e8 = 10^8.
+    vector<vector<int>> dp(N + 1, vector<int>(H + 10001, 1e8));
+    dp[0][0] = 0;
+    int ans = 1e8;
+
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j <= H + 10000; ++j)
+        {
+            dp[i + 1][j] = dp[i][j];
+            if (j >= A[i])
+            {
+                dp[i + 1][j] = min(dp[i + 1][j], dp[i + 1][j - A[i]] + B[i]);
+            }
+
+            if (j >= H)
+            {
+                ans = min(ans, dp[i + 1][j]);
+            }
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
